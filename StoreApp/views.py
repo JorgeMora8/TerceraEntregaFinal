8087223879e2b84a6900c6e19f2ebfeb985c3cc2
JorgeMora8 <jorgeandresmm2002@gmail.com>
2023-02-10 10:28:26 -0400

    forms added

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,13 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+#Importacion de formularios para agregar datos
+from .forms.userForm import UserForm
+from .forms.reviewForm import ReviewForm
+from .forms.productForm import ProductForm
 
 # Create your views here.
 #Another message
 def homepage(request): 
-    return render (request,'homepage.html')
+    form = UserForm()
+    if request.method == 'POST': 
+        form = UserForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            return redirect('store')
+    
+    context = {"form": form}
+    return render (request,'homepage.html', context)
 
 def products(request): 
-    return render (request, 'products.html')
+    form = ProductForm()
+    if request.method == 'POST': 
+        form = ProductForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            redirect("store")
+
+    context = {"form": form}
+    return render (request, 'products.html', context)
 
 def reviews(request): 
-    return render(request, 'reviews.html')
+    form = ReviewForm()
+    if request.method == "POST": 
+        form = ReviewForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            redirect('store')
+
+    context = {"form": form}
+    return render(request, 'reviews.html', context)
